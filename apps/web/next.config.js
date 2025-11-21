569135e1c8ed9deee3a1d64b5c34a14b6b2ad0f0
Paurakh <pandeypaurakhraj@gmail.com>
2025-11-21 16:39:55 +0545

    skipping types check and lint check ins build time

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['http://localhost:3000'],
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
 
 export default nextConfig;
