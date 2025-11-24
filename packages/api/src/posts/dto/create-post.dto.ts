269b8a122149de217d051a4d15986f6458862a0a
Paurakh <pandeypaurakhraj@gmail.com>
2025-11-24 14:31:46 +0545

    dto and entities for user and posts to be exported and used in nest controlers, services

--- packages/api/src/posts/dto/create-post.dto.ts
+++ packages/api/src/posts/dto/create-post.dto.ts
@@ -1,6 +1,6 @@
 export class CreatePostDto {
   title: string;
   content?: string;
+  authorId: number; // Frontend sends this
   published?: boolean;
-  authorId: number;
 }
--- packages/api/src/posts/entities/post.entity.ts
+++
@@ -1,7 +0,0 @@
-export class Post {
-  id: number;
-  title: string;
-  content?: string;
-  published: boolean;
-  authorId: number;
-}
