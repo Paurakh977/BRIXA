3f7c80a2da3ca6b2e96166b0054ef94c922c596d
Paurakh <pandeypaurakhraj@gmail.com>
2025-12-05 16:43:03 +0545

    middlewares,cookies and helmets un main nest config and registering the auth and security and other resource in the app related to auth

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,16 +1,39 @@
 import { Module } from '@nestjs/common';
-
-import { LinksModule } from './links/links.module';
-
-import { AppService } from './app.service';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
-import { UsersModule } from './users/users.module';
-import { PostsModule } from './posts/posts.module';
+import { LinksModule } from './links/links.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+import { RolesGuard } from './common/guards/roles.guard';
+import { configuration, validate } from './config/configuration';
 
 @Module({
-  imports: [LinksModule, DatabaseModule, UsersModule, PostsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      load: [configuration],
+      validate,
+      expandVariables: true,
+    }),
+    DatabaseModule,
+    AuthModule,
+    LinksModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AppModule {}
